90f9d6a3a0aef7c148c978b47b12a0c6088f71ee
Bobur <bobur0218programmer@gmail.com>
2024-01-23 10:56:05 +0500

    night-light-mode is done

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-const btnAddNewTask = document.getElementById("btnAddNewTask");
-const body = document.getElementById("body");
-const addNewTask = document.getElementById("addNewTask");
+document.addEventListener("DOMContentLoaded", function() {
+    let checkbox = document.querySelector('.switch input[type = "checkbox"]');
+    let body = document.body;
+    let svgLogo = document.querySelector(".svg-logo");
 
-btnAddNewTask.addEventListener('click', () => {
-    addNewTask.style.display = "block";
-})
+    if (checkbox) {
+        checkbox.addEventListener("change", function () {
+            if (this.checked) {
+                body.classList.add("dark-mode");
+                svgLogo.src = "../icons/icon-kanban-logo-dark.svg";
+            } else {
+                body.classList.remove("dark-mode");
+                svgLogo.src = "../icons/icon-logo-kanban.svg";
+            }
+        });
+    } else {
+        console.error('Checkbox element not found');
+    }
+});
 
 
 
