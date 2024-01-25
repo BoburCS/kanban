557c25d94f9469927b3d548d06a63bd984d3276e
Bobur <bobur0218programmer@gmail.com>
2024-01-25 11:23:19 +0500

    icon sucks

--- css/components/modetoggle/toggle.js
+++ css/components/modetoggle/toggle.js
@@ -1,19 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() 
-{
-    let checkbox = document.querySelector('.switcher input[type = "checkbox"]');
-    let body = document.body;
+let checkbox = document.querySelector('.switcher input[type = "checkbox"]');
+let body = document.body;
+let logoSVG = document.querySelector(".logoSVG");
 
-    if (checkbox) {
-        checkbox.addEventListener("change", function () 
+if (checkbox) 
+{
+	checkbox.addEventListener("change", function () 
+    {
+		if (this.checked) 
         {
-            if (this.checked) 
-            {
-                body.classList.add("dark-mode");
-            } 
-            else 
-            {
-                body.classList.remove("dark-mode");
-            }
-        });
-    } 
-});
+			body.classList.add("dark-mode");
+            logoSVG.src = "../../../assets/icons/icon-logo-kanban-dark.svg";
+		} 
+        else 
+        {
+			body.classList.remove("dark-mode");
+            logoSVG.src = "../../../assets/icons/icon-logo-kanban.svg";
+		}
+	});
+}
