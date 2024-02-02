cfea5c32b2dde9b8ca9088a256e7badcb4b2ac07
Muhammadbobur Abdukarimov <117216705+BoburCS@users.noreply.github.com>
2024-02-02 13:32:06 +0500

    working on sidebar

--- js/components/sidebar.js
+++ js/components/sidebar.js
@@ -1,5 +1,12 @@
 "use strict";
 
+let boardsTitle = 
+[
+    {title: "Platform Launch"},
+    {title: "Marketing Plan"},
+    {title: "Roadmap"}
+];
+
 const createEl = (type, className) =>
 {
     let element = document.createElement(type);
@@ -7,9 +14,37 @@ const createEl = (type, className) =>
     return element;
 }
 
+const sidebarTop = function ()
+{
+    let sidebarTop = createEl("div", "sidebar-top");
+
+    let h4 = createEl("h4", "heading-s title");
+    h4.textContent = `ALL BOARDS (${boardsTitle.length})`;
+    sidebarTop.append(h4);
+
+    boardsTitle.map(item => 
+    {
+        let board = createEl("button", "board heading-m");
+        board.textContent = `${item.title}`;
+        sidebarTop.append(board);
+    });
+
+    return sidebarTop;
+}
+
+const sidebarBottom = function ()
+{
+    let sidebarBottom = createEl("div");
+
+    return sidebarBottom;
+}
+
 const sidebarContainer = function ()
 {
-    
+    let sidebar = createEl("aside", "sidebar");
+
+    sidebar.append(sidebarTop(), sidebarBottom());
+    return sidebar;
 }
 
 export { sidebarContainer };
